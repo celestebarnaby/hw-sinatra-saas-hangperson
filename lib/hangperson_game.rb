d0688f5f8490703f7e085b5ce4e6793202fba7a2
Celeste Barnaby <cbarnaby@wesleyan.edu>
2016-02-02 23:07:28 +0000

    made game

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,52 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+  
+  def guess(letter)
+    throw :inputError unless letter =~ /\w/
+    nletter = letter.downcase
+    if @guesses.include?(nletter) || @wrong_guesses.include?(nletter) 
+      return false
+    elsif
+      @word.include?(nletter) && !(@guesses.include?(nletter))
+      @guesses += nletter
+      return true
+    elsif !(@wrong_guesses.include?(nletter))
+      @wrong_guesses += nletter
+      return true
+    end
+  end
+  
+  def word_with_guesses
+    word_arr = @word.split(//)
+    gword = ""
+    word_arr.each do |letter|
+      if @guesses.include?(letter)
+        gword+=letter
+      else
+        gword+="-"
+      end
+    end
+    return gword
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length > 6
+      return :lose
+    elsif word_with_guesses == @word
+      return :win
+    else
+      return :play
+    end
   end
 
   def self.get_random_word
